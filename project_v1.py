b1def890aedb82c3b29a3dd22ef7d1f030c92ee1
Elena Hernandez <eahernandez1212@gmail.com>
2024-06-20 21:57:50 +0000

    Loaded API data into database

--- project_v1.py
+++ project_v1.py
@@ -1,14 +1,13 @@
-"""import requests
-url = "https://developers.google.com/apis-explorer/#p/youtube/v3/youtube.activities.list?"
-response = requests.post(url)
-print(response.json())"""
-
+import requests
+import sqlalchemy as db
+import pandas as pd
 # API client library
 import googleapiclient.discovery
+import pprint
 # API information
 api_service_name = "youtube"
 api_version = "v3"
-DEVELOPER_KEY = 'enter key here'
+DEVELOPER_KEY = 'insert API key'
 # API client
 youtube = googleapiclient.discovery.build(
         api_service_name, api_version, developerKey = DEVELOPER_KEY)
@@ -23,4 +22,12 @@ request = youtube.search().list(
 )
 # Request execution
 response = request.execute()
-print(response)
+response2 = {k: v for k, v in response.items() if k != "items"}
+pprint.pp(response2)
+
+dataframe = pd.DataFrame.from_dict(response2)
+engine = db.create_engine('sqlite:///data_base_name.db')
+dataframe.to_sql('table_name', con=engine, if_exists='replace', index=False)
+with engine.connect() as connection:
+   query_result = connection.execute(db.text("SELECT * FROM table_name;")).fetchall()
+   print(pd.DataFrame(query_result))
